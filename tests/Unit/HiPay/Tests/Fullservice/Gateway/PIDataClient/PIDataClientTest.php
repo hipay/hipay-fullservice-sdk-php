a29975e7411d6b3d17938d7ec84d86902b989dce
Theo Viardin <tviardin@hipay.com>
2021-10-25 10:21:53 +0200

    Update CI

--- tests/Unit/HiPay/Tests/Fullservice/Gateway/PIDataClient/PIDataClientTest.php
+++ tests/Unit/HiPay/Tests/Fullservice/Gateway/PIDataClient/PIDataClientTest.php
@@ -239,8 +239,8 @@ class PIDataClientTest extends TestCase
         $params = $dataClient->getHPaymentData($dataId, $hostedPaymentPageRequest, $transaction);
         $this->assertTrue(!empty($params['monitoring']['date_request']));
         $this->assertTrue(!empty($params['monitoring']['date_response']));
-
-        if (version_compare(phpversion(), '<', '7.3.0')) {
+        
+        if (version_compare(phpversion(), '7.3.0', '<')) {
             $this->assertRegExp('/[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]{3}Z/', $params['monitoring']['date_request']);
             $this->assertRegExp('/[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]{3}Z/', $params['monitoring']['date_response']);
         } else {
